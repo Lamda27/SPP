785e2ea2250f935fe292322a5fb9fb4fdbb78d1d
dangelo <laura.dangelo@web.de>
2018-11-28 15:23:13 +0100

    Aufgabe 1 fertiggestellt und getestet: läuft.

--- praktikum1/hello-openmp.c
+++ praktikum1/hello-openmp.c
@@ -1,1 +1,20 @@
-int main ( int argc, char *argv[] ) {}
+#include <stdio.h>
+#include <omp.h>
+int main ( int argc, char *argv[] ) {
+	printf("Exercise 1\n");
+
+	// get maximal number of threads
+	int numMaxThreads = omp_get_max_threads();
+	printf("The number of maximal threads is %i\n",numMaxThreads);
+
+	// set own number of threads
+	int ownNumThreads = 4;
+	omp_set_num_threads(ownNumThreads);
+
+	// create team of threads printing Hello World and its identifier
+	#pragma omp parallel
+	{
+		int threadId = omp_get_thread_num();
+		printf("Hello %i\n",threadId);
+	}
+}
