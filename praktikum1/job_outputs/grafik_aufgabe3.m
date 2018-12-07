4a3900538881357a166661f1627a61262fca5eb9
dangelo <laura.dangelo@web.de>
2018-12-07 09:21:42 +0100

    Job-Outputs von Aufgabe 1 und 2 hochgeladen sowie Grafikerstellungsskript für Aufgabe 3.

---
+++ praktikum1/job_outputs/grafik_aufgabe3.m
@@ -0,0 +1,16 @@
+%% Grafische Darstellungen für Aufgabe 3
+laufzeiten = [0.0121569330; % sequentiell
+    0.0120733980; % parallel (1 Thread)
+    0.0086913200; % parallel (2 Threads)
+    0.0052090220; % parallel (4 Threads)
+    0.0050150550; % parallel (8 Threads)
+    0.0051945020]; % parallel (16 Threads)
+
+threads = [0,1,2,4,8,16];
+
+% bar plot
+figure(1);
+bar(threads,laufzeiten*1000);
+xticklabels({'Seq.','1','2','4','8','16'});
+xlabel('Anzahl paralleler Threads');
+ylabel('Laufzeit (ms)');
