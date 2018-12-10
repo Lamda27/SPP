c72a9cf849207ecb76f8010b11af1baf69faa852
marcelgeibel <marcel-geibel@hotmail.de>
2018-12-10 13:28:58 +0100

    Final
    Kommentare angepasst und schön gemacht

--- praktikum1/quickSortSequential.c
+++ praktikum1/quickSortSequential.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // This function partitions array A into two subarrays A_1 and A_2
 // Input:
@@ -17,19 +18,16 @@
 //     A_1              A_2
 //     [  |  | ... |  ] [  |  | ... |  ]
 //                  *r   *l
-void partition ( int* A, int* l, int* r )
+void partition(int* A, int* l, int* r)
 {
 	int p = A[*r]; // choose last element as pivot
-
-	printf("pivot is ");
-	printf("%d", p);
-	printf("\n");
-
 	int i, tmp;
-	int j = *l-1;
+	int j = *l - 1;
 
-	for(i = *l; i < *r; i++) {
-		if(A[i] < p) {
+	//find elements to swap
+	for (i = *l; i < *r; i++) {
+		//swap elements
+		if (A[i] < p) {
 			j++;
 			tmp = A[j];
 			A[j] = A[i];
@@ -40,8 +38,8 @@ void partition ( int* A, int* l, int* r )
 	tmp = A[j];
 	A[j] = p;
 	A[*r] = tmp;
-	*r = j-1;
-	*l = j+1;
+	*r = j - 1;
+	*l = j + 1;
 }
 
 // Input:
@@ -51,19 +49,13 @@ void partition ( int* A, int* l, int* r )
 //     A
 //     [  |  |  |  |  ...  |  |  |  |  ]
 //      l                            r
-void quicksort( int* A, int l, int r )
+void quicksort(int* A, int l, int r)
 {
 	int first = l, last = r;
-	//quicksort(A,first,last);
 	if (l < r) {
-		partition(A, &l, &r); // sollte Werte f�r l und r �ndern
-		printf("l is ");
-		printf("%d", l);
-		printf("\n");
-		printf("r is ");
-		printf("%d", r);
-		printf("\n");
-		//HIER LIEGT PROBLEM NR 2, da sich die Werte f�r r und l nicht �ndern
+		partition(A, &l, &r);
+
+		//recursion
 		quicksort(A, first, r);
 		quicksort(A, l, last);
 	}
@@ -79,38 +71,26 @@ int main(int argc, char** argv)
 	}
 	// Read number of elements
 	int nrOfElements = atoi(argv[1]);
-    srand( 14811 );
+	srand(14811);
 
-    // Allocate array
+	// Allocate array
 	int* elements;
 	elements = (int *)malloc(nrOfElements * sizeof(int));
 
-	printf("Elements before sorting \n");
-    // Initialize array
+	// Initialize array
 	for (int i = 0; i < nrOfElements; i++) {
 		elements[i] = rand();
-		printf("%d", elements[i]);
-		printf("\n");
 	}
-
+	
 	// RUN QUICKSORT
+	clock_t begin = clock(); //start timer
 	quicksort(elements, 0, nrOfElements - 1);
 
-	//show elements after sorting
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("Elements after sorting \n");
-	for (int i = 0; i < nrOfElements; i++) {
-		printf("%d", elements[i]);
-		printf("\n");
-	}
+	// Time the execution
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-    // Time the execution
-	// TODO
-
-    // Verify sorted order
-
+	// Verify sorted order
 	//check until second to last element
 	for (int i = 0; i < nrOfElements - 1; i++) {
 		if (elements[i] > elements[i + 1])
@@ -122,5 +102,10 @@ int main(int argc, char** argv)
 
 	printf("Sorted correctly? 1 = y, 0 = n: ");
 	printf("%d", sortedCorrectly);
-    return 0;
+	printf("\n");
+	printf("Time for sorting only: ");
+	printf("%f", time_spent);
+	printf(" s");
+
+	return 0;
 }
