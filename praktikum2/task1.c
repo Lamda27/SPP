332638398f422e9491c391a25cfa7c75b071c1d8
dangelo <laura.dangelo@web.de>
2019-01-10 11:32:15 +0100

    Zwischenstand Task 1

--- praktikum2/task1.c
+++ praktikum2/task1.c
@@ -28,7 +28,7 @@ int is_arr_sorted( int* arr, int len ) {
 
 
 /**
- * Checks whether arr is sorted globally. 
+ * Checks whether arr is sorted globally.
  **/
 int verify_results( int* arr, int len, int myrank, int nprocs ) {
 
@@ -64,7 +64,7 @@ int get_unique_rand_elem( unsigned int total_count_bits, unsigned int index, dou
  * Initilizes the input. Each process will have a random local array of numbers. The
  * length of this array is anywhere between 0 to MAX_NUM_LOCAL_ELEMS
  */
-void init_input( int w_myrank, int w_nprocs, int* input_arr, 
+void init_input( int w_myrank, int w_nprocs, int* input_arr,
                  int* input_len, int* total_elems ) {
 
     // Initialize random seed
@@ -131,7 +131,7 @@ void init_input( int w_myrank, int w_nprocs, int* input_arr,
     MPI_Scatter( sendcnts, 1, MPI_INT, input_len, 1, MPI_INT, 0, MPI_COMM_WORLD );
 
     // Redistribute the input
-    MPI_Scatterv( global_arr, sendcnts, displs, MPI_INT, input_arr, *input_len, 
+    MPI_Scatterv( global_arr, sendcnts, displs, MPI_INT, input_arr, *input_len,
                   MPI_INT, 0, MPI_COMM_WORLD );
 
     free( global_arr );
@@ -141,7 +141,7 @@ void init_input( int w_myrank, int w_nprocs, int* input_arr,
 
 int main( int argc, char** argv ) {
 
-	int w_myrank, w_nprocs;
+	  int w_myrank, w_nprocs;
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &w_myrank );
     MPI_Comm_size( MPI_COMM_WORLD, &w_nprocs );
@@ -160,36 +160,69 @@ int main( int argc, char** argv ) {
 
     double start = get_clock_time();
 
+    // Task 1.1: Creating communicators for rows and columns
+    int d = sqrt(w_nprocs); // number of rows and columns
+    int my_row = w_myrank / d;
+    int my_col = w_myrank % d;
+    MPI_Comm comm_row, comm_col;
+    MPI_Comm_split(MPI_COMM_WORLD, my_row, 0, &comm_row);
+    MPI_Comm_split(MPI_COMM_WORLD, my_col, 0, &comm_col);
 
+    // Task 1.2: Gathering elements from row and column
+    int row_recvcount, col_recvcount;
+    int* row_arr = malloc( MAX_NUM_LOCAL_ELEMS * d * sizeof(int) );
+    int* col_arr = malloc( MAX_NUM_LOCAL_ELEMS * d * sizeof(int) );
+    MPI_Allgather(elem_arr, n, MPI_INT, row_arr, row_recvcount, MPI_INT, comm_row);
+    MPI_Allgather(elem_arr, n, MPI_INT, col_arr, col_recvcount, MPI_INT, comm_col);
+    int row_n = n + row_recvcount;
+    int col_n = n + col_recvcount;
 
-    
-    //
-    // TODO
-    //
+    // Task 1.3: Sorting elements
+    qsort(row_arr, row_n, sizeof(int), comp_func);
+    qsort(col_arr, col_n, sizeof(int), comp_func);
 
+    // Task 1.4: Computation of the local ranks
+    int* local_ranks = malloc( col_n * sizeof(int) );
+    int col_elem;
+    for(int i = 0; i < col_n; i++) { // loop through column elements
+      col_elem = col_arr[i];
+      // loop through row elements, until there is encountered a row element
+      // bigger than the column element
+      for(int j = 0; j < row_n; j++) {
+        if (row_arr[j] >= col_elem) {
+          local_ranks[i] = j;
+          break;
+        }
+      }
+    }
 
-    //
-    // Redistribute data
+    // Task 1.5: Computation of the global ranks
+    int* global_ranks = malloc( col_n * sizeof(int) );
+    MPI_Allreduce(local_ranks, global_ranks, col_n, MPI_INT, MPI_SUM, comm_col);
+
+    // Task 1.6: Redistribute data
     // Adjust this code to your needs
     //
-    MPI_Request req_arr[MAX_NUM_LOCAL_ELEMS];
-    MPI_Status stat_arr[MAX_NUM_LOCAL_ELEMS];
+    MPI_Request req_arr[col_n];
+    MPI_Status stat_arr[d];
     int n_req = 0;
 
-    MPI_Isend( &(elem_arr[i]), 1, MPI_INT, arr_global_ranks[j], 0, 
-               MPI_COMM_WORLD, req_arr + n_req );
+    for(int i = 0; i < col_n; i++) {
+      MPI_Isend( &(elem_arr[i]), 1, MPI_INT, global_ranks[i], 0, MPI_COMM_WORLD, req_arr + n_req );
+      n_req++;
+    }
 
     // Receive element
-    // TODO
-   
+    int my_element;
+    for (int i = 0; i < d; i++) {
+      MPI_Recv( &my_element, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, stat_arr + i );
+    }
+
     MPI_Waitall( n_req, req_arr, stat_arr );
-
-
-
-
+    printf("Rank %d has element %d.\n",w_myrank,my_element);
 
     //
-    // Measure the execution time after all the steps are finished, 
+    // Measure the execution time after all the steps are finished,
     // but before verifying the results
     //
     double elapsed = get_clock_time() - start;
@@ -206,12 +239,12 @@ int main( int argc, char** argv ) {
             printf( "Results incorrect!\n" );
         }
     }
-    
+
     // Get timing - max across all ranks
     double elapsed_global;
-    MPI_Reduce( &elapsed, &elapsed_global, 1, MPI_DOUBLE, 
+    MPI_Reduce( &elapsed, &elapsed_global, 1, MPI_DOUBLE,
                 MPI_MAX, 0, MPI_COMM_WORLD );
-                
+
     if( w_myrank == 0 ) {
         printf( "Elapsed time (ms): %f\n", elapsed_global );
     }
